ab07910cf1d7360b7aaec8fe489e08b18ce94ead
Michael Champlin <mchamplin@sbcglobal.net>
2022-02-26 17:41:03 -0800

    Added more cards.  Have Deal & Reset functions + card count

--- game.js
+++ game.js
@@ -2,38 +2,42 @@ console.log("Hello")
 
 let card_img1 = document.getElementById("c1")
 let card_img2 = document.getElementById("c2")
+let maxCards = 50;
 
-/*
-let card = document.createElement("img");
-    card.src = `assets/clubs-1.png`;
-    card.id = `c1`
-    card.className = `card`
+function dealCards(max_cards, face_up) {
+    for (let i=1; i <= max_cards; i++) {
+        let card = document.createElement("img");
+        card.src = `assets/clubs-${i}.png`;
+        card.id = `c${i}`
+        card.className = `card card${i}`
 
-    //card.setAttribute('style',`left: 2em`)
-    card.addEventListener("click",function() {sayHello(card.id)})
+        if (!face_up) card.src ="assets/card_back.jpg"
 
-    let div_container = document.getElementById("card_group")
-    div_container.appendChild(card); 
-    */
+        card.addEventListener("click",function() {sayHello(card.id)})
 
-
-for (let i=1; i <= 13; i++) {
-    let card = document.createElement("img");
-    card.src = `assets/clubs-${i}.png`;
-    card.id = `c${i}`
-    card.className = `card card${i}`
-
-    //let left_justify = 2 + 6*(i-1);
-    //card.setAttribute('style',`left: ${left_justify}em`)
-    card.addEventListener("click",function() {sayHello(card.id)})
-
-    let div_container = document.getElementById("card_group")
-    div_container.appendChild(card); 
+        let div_container = document.getElementById("card_group")
+        div_container.appendChild(card); 
+    }
+}
+function setOpacity (num) {
+    var s= document.getElementById("c2").style;
+    s.opacity = ( num / 100 );
 }
 
 
+function disappearCard(card_num) {
+    document.getElementById(`c${card_num}`).style.opacity = 0;
+}
 
+function resetCards () {
+    clearOutCards(maxCards)
+}
 
+function clearOutCards(max_cards) {
+    for (let i=1; i <= max_cards; i++) {  console.log("clear " + i)
+        document.getElementById(`c${i}`).remove();
+    }
+}
 
 function setImg(card) { 
     document.getElementById(card).src = '';
@@ -43,24 +47,9 @@ function sayHello(cardId) {
     console.log("HIHI " + cardId)
 }
 
-//background-color: rgb(15, 150, 26);
-
-/*
-<img id="c1" class="card1" src="assets/clubs-01-ace.png"
-                onclick="sayHello()">
-            <img id="c2" class="card2" src="assets/clubs-02-two.png">
-*/
-
-/*
-h1 {
-  text-align: center;
-  position: absolute;
-  top: 0.25em;
-
-  width: 100%;
-  color: rgb(224, 193, 52);
-  font-size: 4em;
-  font-family: "Franklin Gothic Medium";
+function dealOutCards() {
+    selectElement = document.querySelector('#card_selector');
+    console.log("Selected " + selectElement.value);
+    maxCards = selectElement.value;
+    dealCards(selectElement.value,1)
 }
-
-*/
