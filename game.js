6db14e4f9603a0148ad7d8f211f56afd159eb06a
Michael Champlin <mchamplin@sbcglobal.net>
2022-02-26 19:50:11 -0800

    Added shuffle, card selection and comparison stubbed

--- game.js
+++ game.js
@@ -1,29 +1,29 @@
-console.log("Hello")
-
-let card_img1 = document.getElementById("c1")
-let card_img2 = document.getElementById("c2")
 let maxCards = 50;
+let cardLocationToID = [100]
+let activeCardIndex = null;
 
-function dealCards(max_cards, face_up) {
-    for (let i=1; i <= max_cards; i++) {
+function dealCards(max_cards) {
+    console.log("Deal it")
+    for (let i=0; i < max_cards; i++) {
         let card = document.createElement("img");
-        card.src = `assets/clubs-${i}.png`;
-        card.id = `c${i}`
-        card.className = `card card${i}`
+        let card_id = cardLocationToID[i].id;
+        console.log("ID = " + card_id)
 
-        if (!face_up) card.src ="assets/card_back.jpg"
+        let card_image = `assets/clubs-${card_id}.png`;
 
-        card.addEventListener("click",function() {sayHello(card.id)})
+        card.src = "assets/card_back.jpg";
+        card.id = `c${card_id}`
+        card.className = `card card${card_id}`
+
+        card.addEventListener("click",function() {cardClicked(i)})
 
         let div_container = document.getElementById("card_group")
         div_container.appendChild(card); 
+
+        cardLocationToID[i].element = card;
+        cardLocationToID[i].img     = card_image;
     }
 }
-function setOpacity (num) {
-    var s= document.getElementById("c2").style;
-    s.opacity = ( num / 100 );
-}
-
 
 function disappearCard(card_num) {
     document.getElementById(`c${card_num}`).style.opacity = 0;
@@ -34,22 +34,61 @@ function resetCards () {
 }
 
 function clearOutCards(max_cards) {
-    for (let i=1; i <= max_cards; i++) {  console.log("clear " + i)
-        document.getElementById(`c${i}`).remove();
+    console.log("Clearing a total of " + max_cards)
+    for (let i=0; i < max_cards; i++) {  console.log("clear " + i)
+        cardLocationToID[i].element.remove();
+        cardLocationToID[i] = null;
     }
+    activeCardIndex = null;
 }
 
-function setImg(card) { 
-    document.getElementById(card).src = '';
-}
+function cardClicked(card) {
+    cardLocationToID[card].element.src = cardLocationToID[card].img;
 
-function sayHello(cardId) {
-    console.log("HIHI " + cardId)
+
+    if (activeCardIndex) {
+        console.log("Curr Card = " + cardLocationToID[card].element.id + " Prev Card = " + cardLocationToID[activeCardIndex].element.id);
+
+        if (cardLocationToID[card].element.id === cardLocationToID[activeCardIndex].element.id)
+            console.log("MATCH!")
+        activeCardIndex = null;
+    } else {
+        activeCardIndex = card;
+    }
 }
 
 function dealOutCards() {
     selectElement = document.querySelector('#card_selector');
-    console.log("Selected " + selectElement.value);
     maxCards = selectElement.value;
-    dealCards(selectElement.value,1)
+    cardLocationToID.length = maxCards;
+
+    shuffle();
+
+    dealCards(maxCards)
+}
+
+function shuffleArray() {
+    for (let i = 0; i < cardLocationToID.length; i++) {
+        let card = null;
+        let img = null;
+        let card_object = {element:card, image:img, id: Math.floor(i/2)+1}
+
+        cardLocationToID[i] = card_object;
+    }
+    for (let i = cardLocationToID.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = cardLocationToID[i];
+        cardLocationToID[i] = cardLocationToID[j];
+        cardLocationToID[j] = temp;
+    }
+}
+
+function shuffle() {
+    console.log("Shuffle")
+    shuffleArray()
+    console.log("Shuffled:")
+    for (let i=0; i < cardLocationToID.length; i++) {
+        console.log("New Location for " + i + " is " + cardLocationToID[i].id)
+        //cardLocationToID[i].element.src = cardLocationToID[i].element.image;
+    }
 }
